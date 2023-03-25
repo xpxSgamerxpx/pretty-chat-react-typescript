398fded8c017ba9d8dc6084779f671200bd16063
shlok <69759801+xpxSgamerxpx@users.noreply.github.com>
2023-03-25 10:42:05 -0400

    Update SignUpForm.tsx

--- src/AuthPage/SignUpForm.tsx
+++ src/AuthPage/SignUpForm.tsx
@@ -80,7 +80,7 @@ const SignUpForm = (props: SignUpFormProps) => {
         <TextInput
           label="First name"
           name="first_name"
-          placeholder="Adam"
+          placeholder="shlok"
           style={{ width: isMobile ? "100%" : "calc(50% - 6px)" }}
           onChange={(e) => setFirstName(e.target.value)}
         />
@@ -88,7 +88,7 @@ const SignUpForm = (props: SignUpFormProps) => {
         <TextInput
           label="Last name"
           name="last_name"
-          placeholder="La Morre"
+          placeholder="batavia"
           style={{
             width: isMobile ? "100%" : "calc(50% - 6px)",
             float: "right",
@@ -99,7 +99,7 @@ const SignUpForm = (props: SignUpFormProps) => {
         <TextInput
           label="Email"
           name="email"
-          placeholder="adam@lamorre.co"
+          placeholder="shlokbatavia@gmail.com"
           style={{ width: isMobile ? "100%" : "calc(50% - 6px)" }}
           onChange={(e) => setEmail(e.target.value)}
         />
