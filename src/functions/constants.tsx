a9a733b9ce792020fc6ce728df16bde58b70cfa3
shlok <69759801+xpxSgamerxpx@users.noreply.github.com>
2023-03-25 14:12:40 -0400

    Update constants.tsx

--- src/functions/constants.tsx
+++ src/functions/constants.tsx
@@ -1,7 +1,7 @@
 export const projectId = process.env.REACT_APP_PROJECT_ID
   ? process.env.REACT_APP_PROJECT_ID
-  : "8bbc1d42-8250-46a0-b91e-b3b3d371eb72";
+  : "0644f678-c277-4b27-9f35-73ba9e9bf03f";
 
 export const privateKey: string = process.env.REACT_APP_PROJECT_KEY
   ? process.env.REACT_APP_PROJECT_KEY
-  : "f962993d-aecf-4832-90c6-7b9e0f7372a4";
+  : "0644f678-c277-4b27-9f35-73ba9e9bf03f";
